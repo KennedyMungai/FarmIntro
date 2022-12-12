a1568482d242161a291902cd12d3a39c17b7c2b1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-12 22:06:50 +0300

    Added a h1 inside the div

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function App()
 {
   return (
-    <div className="App list-group-item justify-content-center align-items-center mx-auto">
-
+    <div className="App list-group-item justify-content-center align-items-center mx-auto"
+      style={{ "width": "400px", "background-color": "white", "margin-top": "15px" }}>
+      <h1 className="card text-white bg-primary mb-1" styleName="max-width: 20rem;">Task Manager</h1>
     </div>
   );
 }
