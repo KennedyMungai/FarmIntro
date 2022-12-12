da8385224a35c369d2264e98b3e8b1d7c74b0ccb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-12 17:34:28 +0300

    Defined a function to fetch all todos

--- backend/database.py
+++ backend/database.py
@@ -15,3 +15,13 @@ collection = database.todo
 async def fetch_one_todo(title):
     document = await collection.find_one({"title": title})
     return document
+
+
+async def fetch_all_todos():
+    todos = []
+    cursor = collection.find({})
+
+    async for document in cursor:
+        todos.append(Todo(**document))
+
+    return todos
