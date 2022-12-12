0b90a432a610e9fd386378f8a7c1077909a3d5dd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-12 22:12:27 +0300

    Added two input fields

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,10 @@ function App()
       <h6 className="card text-white bg-primary mb-3">FASTAPI - React - MongoDB</h6>
       <div className="card-body">
         <div className="card text-white bg-dark mb-3">Add your tasks</div>
+        <span className="card-text">
+          <input type="text" className="mb-2 form-control titleIn" placeholder='Title' />
+          <input type="text" className="mb-2 form-control desIn" placeholder='Description' />
+        </span>
       </div>
     </div>
   );
