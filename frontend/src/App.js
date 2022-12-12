6bc9be4ff74d7632fb9cc5f8c80be80f24cf3b4e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-12 22:23:30 +0300

    Made a series of changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
@@ -8,8 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function App()
 {
   return (
-    <div className="App list-group-item justify-content-center align-items-center mx-auto"
-      style={{ "width": "400px", "background-color": "white", "margin-top": "15px" }}>
+    <div className="App list-group-item justify-content-center align-items-center mx-auto" style={{ "width": "400px", "background-color": "white", "margin-top": "15px" }}>
       <h1 className="card text-white bg-primary mb-1" styleName="max-width: 20rem;">Task Manager</h1>
       <h6 className="card text-white bg-primary mb-3">FASTAPI - React - MongoDB</h6>
       <div className="card-body">
@@ -19,6 +17,10 @@ function App()
           <input type="text" className="mb-2 form-control desIn" placeholder='Description' />
           <button className="btn btn-outline-primary mx-2" style={{ 'border-radius': '50px', 'font-weight': 'bold' }}>Add Task</button>
         </span>
+        <h5 className="card text-white bg-dark my-3"> Your Tasks</h5>
+        <div>
+          {/* Todo items */}
+        </div>
       </div>
     </div >
   );
