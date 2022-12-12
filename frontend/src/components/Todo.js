0e35ce0994b16ced6816d8d68f14217c9f3b1eaf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-12 23:03:36 +0300

    Took care of an syntax error

--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -2,11 +2,11 @@ import axios from 'axios'
 import React from 'react'
 
 
-const TodoItem = (props) => 
+function TodoItem(props)
 {
     const deleteTodoHandler = (title) => 
     {
         axios.delete(`http://localhost:8000/api/todo/${title}`)
-            .then(res => console.log(res.data)
+            .then(res => console.log(res.data))
     }
 }
