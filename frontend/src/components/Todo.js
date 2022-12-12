fb5068e16ce13a0f39e4a08c0b80a19abe4abea2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-12 23:10:46 +0300

    Exported the TodoItem function

--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -8,5 +8,23 @@ const TodoItem = (props) =>
     {
         axios.delete(`http://localhost:8000/api/todo/${title}`)
             .then(res => console.log(res.data))
+
+        return (
+            <div>
+                <p>
+                    <span style={{ fontWeight: 'bold, underline' }}>{props.todo.title} : </span> {props.todo.description}
+                    <button
+                        className="btn btn-outline-danger m2"
+                        onClick={() => deleteTodoHandler(props.todo.title)}
+                        style={'borderRadius: 50px'}
+                    >
+                        X
+                    </button>
+                    <hr />
+                </p>
+            </div>
+        )
     }
 }
+
+export default TodoItem;
