1eab7ba5462c1f4e326d7246b18bcb65ec6b5f96
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-12 17:31:39 +0300

    Wrote the function to get one todo

--- backend/database.py
+++ backend/database.py
@@ -10,3 +10,8 @@ client = motor.motor_async_io.AsyncIOMotorClient('mongodb://localhost:27017')
 database = client.TodoList
 
 collection = database.todo
+
+
+async def fetch_one_todo(title):
+    document = await collection.find_one({"title": title})
+    return document
