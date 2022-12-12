677e5ff2ec8aa84eec1971c6a32edcc442a87fe2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-12 22:30:17 +0300

    Defined the todoList, title and desc states

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App()
 {
+  const [todoList, setTodoList] = useState('')
+  const [title, setTitle] = useState('')
+  const [desc, setDesc] = useState('')
+
   return (
     <div className="App list-group-item justify-content-center align-items-center mx-auto" style={{ "width": "400px", "background-color": "white", "margin-top": "15px" }}>
       <h1 className="card text-white bg-primary mb-1" styleName="max-width: 20rem;">Task Manager</h1>
