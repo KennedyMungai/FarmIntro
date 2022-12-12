a0a029f9ffa09a6a4604d4e0949ed0ddfc5d563b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-12 22:01:04 +0300

    Addd an import for bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 function App()
