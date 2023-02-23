b1e0e916fac597560d785c2306eba0a73e4d3852
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-23 10:43:38 +0300

    Added documentation for the various API endpoints

--- backend/main.py
+++ backend/main.py
@@ -17,29 +17,72 @@ app.add_middleware(
 
 @app.get("/")
 def read_root():
+    """A simple endpoint used to test API connection
+
+    Returns:
+        object: A simple dumb object
+    """
     return {"Ping": "Pong"}
 
 
 @app.get('/api/todo')
 async def get_todo():
+    """Am API endpoint intended for retrieving all todos
+
+    Returns:
+        1: A number (for testing purposes)
+    """
     return 1
 
 
 @app.get('/api/todo{id}')
 async def get_todo_by_id(id):
+    """An endpint for retrieving individual todos by their id
+
+    Args:
+        id (identifier): An identifier for individual todos
+
+    Returns:
+        1: A simple number
+    """
     return 1
 
 
 @app.post('/api/todo')
 async def post_todo(todo):
+    """A simple endpoint for posting todos
+
+    Args:
+        todo (Object): The actual item being posted
+
+    Returns:
+        1: For testing purposes
+    """
     return 1
 
 
 @app.put('/api/todo{id}')
 async def put_todo(id, data):
+    """An endpoint for updating the todos
+
+    Args:
+        id (identifier): Acts as the identifier for the todo to be updated
+        data (Object): The object meant to replace the todo
+
+    Returns:
+        1: A number for testing purposes
+    """
     return 1
 
 
 @app.delete('/api/todo{id}')
 async def delete_todo(id):
+    """A simple endpoint intended for deleting todos
+
+    Args:
+        id (Identifier): An identifier for the todo to be deleted
+
+    Returns:
+        1: A simple number for testing purposes
+    """
     return 1
