6876cac58ea96a9c761e74a82f66e297b029b5bc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-23 12:45:08 +0300

    Got rid of an extra div

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,30 +5,28 @@ import axios from 'axios'
 function App()
 {
   return (
-    <div className="App">
-      <div
-        className="list-group-item justify-content-center align-items-center mx-auto"
+    <div
+      className="list-group-item justify-content-center align-items-center mx-auto"
+      style={
+        {
+          "width": "400px",
+          "backgroundColor": "white",
+          "marginTop": "15px",
+          "textAlign": "center"
+        }
+      }
+    >
+      <h1
+        className="cart text-white bg-primary mb"
         style={
           {
-            "width": "400px",
-            "backgroundColor": "white",
-            "marginTop": "15px",
-            "textAlign": "center"
+            "padding": "1rem"
           }
         }
       >
-        <h1
-          className="cart text-white bg-primary mb"
-          style={
-            {
-              "padding": "1rem"
-            }
-          }
-        >
-          Task Manager
-        </h1>
-        <h6 className="card text-white bg-primary mb-3">FARM boi!</h6>
-      </div>
+        Task Manager
+      </h1>
+      <h6 className="card text-white bg-primary mb-3">FARM boi!</h6>
     </div>
   );
 }
