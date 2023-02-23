9fb0589b61ad97393bc959c5df176b564ae9a3d2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-23 10:57:06 +0300

    Fetched and returned the document

--- backend/database.py
+++ backend/database.py
@@ -8,3 +8,13 @@ import motor.motor_asyncio
 client = motor.motor_asyncio.AsyncIOMotorClient(apiKey)
 database = client.TodoList
 collection = database.todo
+
+
+async def fetch_one_todo(title):
+    """An async function that fetches one todo from the db
+
+    Args:
+        title (String): The item used in fetching the todo
+    """
+    document = await collection.find_one({"title": title})
+    return document
