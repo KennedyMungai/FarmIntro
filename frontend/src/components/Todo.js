aba4762ea2c7032b7d870efbf90acb96e0f64f2b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-23 13:47:22 +0300

    Created the deleteTodoHandler function

--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -1,8 +1,14 @@
 import axios from 'axios'
 import React from 'react'
 
-const Todo = () =>
+const Todo = (props) =>
 {
+    const deleteTodoHandler = (todo) =>
+    {
+        axios.delete(`http://localhost:800/api/todo/${title}`)
+            .then((res) => console.log(res.data))
+    }
+
     return (
         <div>Todo</div>
     )
