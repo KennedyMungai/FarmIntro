ade1cbfa5bc82c3e6de9cfbb243e89d603ce0a45
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-23 10:08:38 +0300

    Added middleware to the project

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,11 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins =['https://localhost:3000']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
